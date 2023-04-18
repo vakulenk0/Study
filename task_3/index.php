42b7801962a2d90c5b3c629cb0d988a5aa262831
vakulenk0 <113202090+vakulenk0@users.noreply.github.com>
2023-04-18 20:23:25 +0300

    fix7

--- task_3/index.php
+++ task_3/index.php
@@ -1,95 +1,112 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+// Отправляем браузеру правильную кодировку,
+// файл index.php должен быть в кодировке UTF-8 без BOM.
+header('Content-Type: text/html; charset=UTF-8');
+//$connect = mysqli_connect('localhost', 'u52961', '4288671', 'u52961');
+//if(!mysqli_connect_errno()){
+//    echo 'Не удалось подключиться: ', mysqli_connect_error();
+//}
+//else
+//    echo 'Подключились к бд';*/
 
-<head>
-    <meta charset="UTF-8">
-    <title>Задание 3</title>
-    <link rel="stylesheet" href="style3.css">
-</head>
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+  if (!empty($_GET['save'])) {
+    // Если есть параметр save, то выводим сообщение пользователю.
+    print('Спасибо, результаты сохранены.');
+  }
+  // Включаем содержимое файла form.php.
+  include('form.php');
+  // Завершаем работу скрипта.
+  exit();
+}
 
-<body>
-    <div>
-        <form id="form" action="/" method="POST">
-            <h2 class="pb-4 pt-3 text-left mx-3 text-sm-center">Форма</h2><br>
-      <div class="text-sm-center text-left mx-2" id="form_text">
-        <label>
-          Имя: <br>
-          <input name="name" value="Введите ваше имя"/>
-        </label> <br>
+$errors = FALSE;
+if (empty($_POST['name'])) {
+  print('Некорректное имя.');
+  $errors = TRUE;
+}
 
-        <label>
-          Ваш email: <br>
-          <input name="email" type="email" value="Введите email"/>
-        </label><br>
+if(empty($_POST['email'])){
+  print('Некорректный email.');
+  $errors = TRUE;
+}
 
-        <label >
-          Дата: <br>
-          <input name="data" type="date" value=""/>
-        </label> <br>
+if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
+  print('Некорректная дата.');
+  $errors = TRUE;
+}
 
-        <label>
-          Ваш пол: <br>
-          <input type="radio" name="sex">
-          М
-        </label>
+if(empty($_POST['sex'])){
+  print('Отметьте ваш пол.');
+  $errors = TRUE;
+}
 
-        <label >
-          <input type="radio" name="sex">
-          Ж
-        </label><br>
+if(empty($_POST['limbs'])){
+  print('Отметьте кол-во конечностей.');
+  $errors = TRUE;
+}
 
-        Кол-во конечностей: <br>
-        <label>
-          <input type="radio" name="limbs">
-          1
-        </label>
-        <label>
-          <input type="radio" name="limbs">
-          2
-        </label>
-        <label>
-          <input type="radio" name="limbs">
-          3
-        </label>
-        <label>
-          <input type="radio" name="limbs">
-          4
-        </label><br>
+if(empty($_POST['abilities'])){
+  print('Отметьте ваши способности.');
+  $errors = TRUE;
+}
 
-        <label>
-          Сверхспособности:
-          <br >
-          <select name="abilities" multiple="multiple">
-            <option>
-              Бессмертие
-            </option>
-            <option >
-              Прохождение сквозь стены
-            </option>
-            <option >
-              Левитация
-            </option>
+if(empty($_POST['biography'])){
+  print('Введите начало своей биографии.');
+  $errors = TRUE;
+}
 
-          </select>
-        </label><br />
+if(empty($_POST['rules'])){
+  print('Вы не согласились с контрактом.');
+  $errors = TRUE;
+}
 
-        <label >
-          Биография: <br>
-          <textarea name="biography" >Введите начало своей биографии
-        </textarea>
-        </label><br>
+if($errors) exit();
 
-        <label >
-          <input type="checkbox" name="rules">
-        </label>
+$user = 'u52961'; // Заменить на ваш логин uXXXXX
+$pass = '4288671'; // Заменить на пароль, такой же, как от SSH
+$db = new PDO("mysql:host=localhost;dbname=$user", $user, $pass,
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
 
-        С контрактом ознакомлен (а)<br>
-        <label >
-          <input type="submit" value="Отправить">
-        </label>
-      </div>
-        </form>
-    </div>
+// Подготовленный запрос. Не именованные метки.
+try {
+  $stmt = $db->prepare('INSERT INTO application ("name", "email", "data", "sex", "limbs", "biography") VALUES ($_POST["name"],
+    $_POST["email"],$_POST["data"],$_POST["sex"],$_POST["limbs"],$_POST["biography"])');
+  $stmt->execute([$_POST['name']]);
+}
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
 
-</body>
-</html>
+$name = $_POST['name'];
+
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+
+//  stmt - это "дескриптор состояния".
+
+//  Именованные метки.
+//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
+//$stmt -> execute(['label'=>'perfect', 'color'=>'green']);
+
+//Еще вариант
+/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
+$stmt->bindParam(':firstname', $firstname);
+$stmt->bindParam(':lastname', $lastname);
+$stmt->bindParam(':email', $email);
+$firstname = "John";
+$lastname = "Smith";
+$email = "john@test.com";
+$stmt->execute();
+*/
+
+// Делаем перенаправление.
+// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
+// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
+header('Location: ?save=1');
+?>
